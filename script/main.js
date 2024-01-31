7b536c439108dd9f53e4148c237f3dd5b19706d6
Eric Park <ericpark112@gmail.com>
2024-01-30 23:42:51 -0800

    Additional Updates

--- script/main.js
+++ script/main.js
@@ -35,10 +35,6 @@ const animationTimeline = () => {
       opacity: 0,
       y: 10,
     })
-    .from(".two", 0.4, {
-      opacity: 0,
-      y: 10,
-    })
     .to(
       ".one",
       0.7,
@@ -47,15 +43,6 @@ const animationTimeline = () => {
         y: 10,
       },
       "+=2.5"
-    )
-    .to(
-      ".two",
-      0.7,
-      {
-        opacity: 0,
-        y: 10,
-      },
-      "-=1"
     )
     .from(".three", 0.7, {
       opacity: 0,
