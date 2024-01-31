91de9ffb8027122a6a349bf9d9f470ac122a4c0b
Eric Park <ericpark112@gmail.com>
2024-01-31 01:09:05 -0800

    change hidden elements

--- script/toggle.js
+++ script/toggle.js
@@ -1,13 +1,13 @@
 
-const elementToToggle = document.getElementById('elementToToggle');
+const yesElement = document.getElementById('yesElement');
 const noElement = document.getElementById('noElement');
 
 const yesBtn = document.getElementById("btnYes");
 yesBtn.addEventListener("click", () => {
-  elementToToggle.classList.toggle('hidden');
+  yesElement.classList.toggle('hiddenYes');
 })
 
 const noBtn = document.getElementById("btnYes");
 noBtn.addEventListener("click", () => {
-  noElement.classList.toggle('hidden');
+  noElement.classList.toggle('hiddenNo');
 })
